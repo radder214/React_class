e3efe4a4d0b0fa97ced4362a9b9c445836993088
radder214 <garden940316@gmail.com>
2025-09-09 22:59:53 +0900

    JSP & Props

--- movie_app/src/App.js
+++ movie_app/src/App.js
@@ -1,6 +1,30 @@
+import foodILike from "./foods";
+import PropTypes from "prop-types";
+
+function Food({name, image, rating}) { // 구조 분해 할당
+  return (
+    <div>
+      <h2>I like {name}, {rating}/5</h2>
+      <img src={image} alt={name} width="100" height="100"/>
+    </div>
+  );
+}
+Food.propTypes = {
+  name: PropTypes.string.isRequired,
+  image: PropTypes.string.isRequired,
+  rating: PropTypes.number.isRequired
+}
+
 function App() {
   return (
-    <div>123</div>
+    <div>
+      {
+        foodILike.map((item, index) => 
+          <Food name={item.name} image={item.image} rating={item.rating} key={index}/>
+        )
+      }
+      {/* cf) <Food name={"Rice"}/> = <Food name="Rice"/> */}
+    </div>
   );
 }
 
